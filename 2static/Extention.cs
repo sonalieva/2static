91b46453124b8a12e2f3126299a089e633128fc4
sonalieva <708v8lg@code.edu.az>
2022-04-06 10:02:05 +0400

    created

---
+++ 2static/Extention.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2static
+{
+    public static class Extention
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsContainsDigit(this string smth)
+        {
+            foreach (var item in smth)
+            {
+                if (char.IsDigit(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+       
+        public static int[] GetValueIndexes(this string smth, char chr)
+        {
+            int[] arr = new int[0];
+            for (int i = 0; i < smth.Length; i++)
+            {
+                if (smth[i] == chr)
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = i;
+                }
+            }
+            return arr;
+        }
+    }
+}
---
+++ 2static/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
